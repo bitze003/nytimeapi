5b60aabe788a37a2486ce17edb56779333d0ff84
Kenneth Bitzer <bitze003@d.umn.edu>
2018-09-27 22:28:39 -0500

    moved over files from my broken repository

---
+++ app/components/common/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <hr />
+    <p className="pull-right">
+      
+     NY API 
+    </p>
+  </footer>
+);
+
+export default Footer;
